6754eae36c0e78e77bcb0b13f81791f5e8a00416
Jake Romer <mail@jakeromer.com>
2020-03-20 19:45:18 -0400

    Add randomInt utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as FillGrid } from './fill-grid'
+export { default as randomInt } from './random-int'
 export { default as shuffle } from './shuffle'
---
+++ src/utils/random-int/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Return a random integer in range [floor, ceiling),
+ * where floor is by default 0.
+ *
+ * @param {Number} bound1 if only bound given, upper bound (exclusive), otherwise lower bound (inclusive)
+ * @param {Number?} bound2 upper bound (exclusive)
+ *
+ * @return {Number} a random integer
+ */
+export default function randomInt(bound1: number, bound2: number = null): number {
+    let [lb, ub] = bound2
+        ? [bound1, bound2]
+        : [0, bound1]
+
+    if (ub <= lb) {
+        throw new RangeError('lower bound must be less than upper bound.')
+    }
+
+    return Math.floor(lb + Math.random() * (ub - lb))
+}
