63026a443eb6b2645adbd33f11c3be0130420472
Jake Romer <mail@jakeromer.com>
2020-03-20 22:59:01 -0400

    Fix App component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world text', () => {
+test('renders Sudoku header', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/hello world/i);
+    const linkElement = getByText(/Sudoku/i);
     expect(linkElement).toBeInTheDocument();
 });
