e2ee230186b38ad7e3b71e31a2e50bdf9d3860f5
Jake Romer <mail@jakeromer.com>
2020-03-22 23:14:51 -0400

    Fix duplicate neighborhood entry bug and refactor

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -25,54 +25,39 @@ function isInCol(value: VALUE, grid: GRID, colIndex: INDEX): boolean {
 // coordinate [`row`, `col`].
 function subGridIndexes(row: INDEX, col: INDEX): COORD[] {
     const lowerBound = (n: INDEX): INDEX => (n < 3) ? 0 : (n > 5) ? 6 : 3
-    const baseRow: INDEX = lowerBound(row)
-    const baseCol: INDEX = lowerBound(col)
-
-    const indices: COORD[][] =
-        [...Array(3)].map((_, i) =>
-            [...Array(3)].map((_, j) =>
-                [
-                    (baseRow + i) as INDEX,
-                    (baseCol + j) as INDEX,
-                ] as COORD
-            ) as COORD[])
-
-    const coords: COORD[] = []
-    coords.concat(...indices)
-
-    return coords
+    const baseCoords: COORD = [lowerBound(row), lowerBound(col)]
+    return adjacentSubgridCoords(...baseCoords)
 }
 
+// Given the "base" coordinates of a subgrid (i.e., the row and column of the
+// subgrid's origin), return an array of COORD pairs for all the cells in that
+// subgrid.
+function adjacentSubgridCoords(row: INDEX, col: INDEX): COORD[] {
+    return [
+        [row + 0, col + 0], [row + 0, col + 1], [row + 0, col + 2],
+        [row + 1, col + 0], [row + 1, col + 1], [row + 1, col + 2],
+        [row + 2, col + 0], [row + 2, col + 1], [row + 2, col + 2],
+    ] as COORD[]
+}
+
+// Return a list of COORD lists, each containing the coordinates of a
+// sudoku board subgrid.
 function allSubgridIndexes(): COORD[][] {
-    const baseCoords: COORD[] = [
+    const subgridOriginCoords: COORD[] = [
         [0, 0], [0, 3], [0, 6],
         [3, 0], [3, 3], [3, 6],
         [6, 0], [6, 3], [6, 6],
     ] as COORD[]
-
-    const indices: COORD[][] = baseCoords.map(([row, col]) => [
-        [row + 0, col + 0], [row + 0, col + 1], [row + 0, col + 2],
-        [row + 1, col + 0], [row + 1, col + 1], [row + 1, col + 2],
-        [row + 2, col + 0], [row + 2, col + 1], [row + 2, col + 2],
-    ]) as COORD[][]
-
-    return indices
+    return subgridOriginCoords.map(coords => adjacentSubgridCoords(...coords))
 }
 
 // Is the target value `target` in the neighborhood of
 // [`rowIndex`, `colIndex`] (i.e., in its enclosing sub-grid) of grid `grid`?
 function isInSubGrid(value: VALUE, grid: GRID, rowIndex: INDEX, colIndex: INDEX): boolean {
-    const coords = subGridIndexes(rowIndex, colIndex)
-
-    for (let [row, col] of coords) {
-        if (grid[row][col] === value) {
-            return true
-        }
-    }
-
-    return false
+    const subgridCoords = subGridIndexes(rowIndex, colIndex)
+    const subgridValues = subgridCoords.map(([row, col]) => grid[row][col])
+    return subgridValues.includes(value)
 }
-
 
 // Is the given `grid` completely filled?
 function isValidInPosition(value: VALUE, grid: GRID, rowIndex: INDEX, colIndex: INDEX): boolean {
@@ -81,6 +66,7 @@ function isValidInPosition(value: VALUE, grid: GRID, rowIndex: INDEX, colIndex: 
         && !isInSubGrid(value, grid, rowIndex, colIndex)
 }
 
+// Are the given arrays equivalent by value?
 function equalArrays(arr1: VALUE[], arr2: VALUE[]): boolean {
     return arr1.length === arr2.length
         && arr1.every((value, index) => value === arr2[index])
@@ -143,24 +129,27 @@ export default function fillGrid(grid: GRID): boolean {
         row = Math.floor(i / SUDOKU_GRID_SIZE) as INDEX
         col = i % SUDOKU_GRID_SIZE as INDEX
 
-        // Skip if the position is filled already
-        if (grid[row][col] !== 0) { continue }
+        if (grid[row][col] === 0) {
+            shuffle(values)
 
-        shuffle(values)
+            for (let value of values) {
+                if (!isInRow(value, grid, row)) {
+                    if (!isInCol(value, grid, col)) {
+                        if (!isInSubGrid(value, grid, row, col)) {
+                            grid[row][col] = value
 
-        for (let value of values) {
-            if (isValidInPosition(value, grid, row, col)) {
-                grid[row][col] = value
-
-                if (isFullGrid(grid)) {
-                    return true
-                } else if (fillGrid(grid)) {
-                    return true
+                            if (isFullGrid(grid)) {
+                                return true
+                            } else if (fillGrid(grid)) {
+                                return true
+                            }
+                        }
+                    }
                 }
             }
-        }
 
-        break
+            break
+        }
     }
 
     // reset to 0 if we fail to populate [row, col]
