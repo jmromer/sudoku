94c46d88fa9cb60b742b537b8292fd45a3c9656d
Jake Romer <mail@jakeromer.com>
2020-03-22 22:05:39 -0400

    Add typings file

---
+++ src/typings/index.ts
@@ -0,0 +1,19 @@
+export const SUDOKU_GRID_SIZE = 9
+
+// Valid sudoku values
+export type VALID_VALUE = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+// Valid sudoku values, and 0 to denote an empty space
+export type VALUE = 0 | VALID_VALUE
+
+// A grid row
+export type ROW = [VALUE, VALUE, VALUE, VALUE, VALUE, VALUE, VALUE, VALUE, VALUE]
+
+// A grid
+export type GRID = [ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW]
+
+// A valid grid array index
+export type INDEX = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+// A valid grid coordinate index pair
+export type COORD = [INDEX, INDEX]
