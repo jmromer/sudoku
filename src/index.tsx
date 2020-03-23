e26b54c0cc0d3461d34e30bd59bfd5aa8d91e7f3
Jake Romer <mail@jakeromer.com>
2020-03-23 18:30:44 -0400

    Update import paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-import { unregister } from './core';
+import App from 'App';
+import { unregister } from 'core';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
